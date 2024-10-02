6d1be7a7cc69782a2acd893578abcd4da2156b13
sunggyupaik <melon9751@naver.com>
2024-10-02 19:43:25 +0900

    [feat] 날짜로 특강 조회 목록을 구현하라
    요구사항이 날짜로 조회할 수 있어야 하므로 특강 날짜와 시간을
    별도의 클래스로 분리하였습니다. CourseDate로 JPA에서 조회가 가능합니다.
    
    제대로 조회가 되는지 테스트해보기 위해 @DataJpaTest를 활용해 검증합니다.

---
+++ src/main/java/com/hanghe/enrollment/domain/course/CourseDate.java
@@ -0,0 +1,45 @@
+package com.hanghe.enrollment.domain.course;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Embeddable
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CourseDate {
+    private Integer year;
+    private Integer month;
+    private Integer day;
+
+    @Builder
+    public CourseDate(
+            Integer year,
+            Integer month,
+            Integer day
+    ) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CourseDate that = (CourseDate) o;
+        return Objects.equals(getYear(), that.getYear())
+                && Objects.equals(getMonth(), that.getMonth())
+                && Objects.equals(getDay(), that.getDay());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getYear(), getMonth(), getDay());
+    }
+}
--- src/main/java/com/hanghe/enrollment/domain/course/CourseRepository.java
+++ src/main/java/com/hanghe/enrollment/domain/course/CourseRepository.java
@@ -1,11 +1,7 @@
 package com.hanghe.enrollment.domain.course;
 
-import com.hanghe.enrollment.domain.course.dto.CourseDateRequestDto;
-
 import java.util.List;
 
 public interface CourseRepository {
-    List<Course> findAllByCourseTime(CourseDateRequestDto courseTime);
-
-    Course save(Course course);
+    List<Course> findAllByCourseDate(CourseDate courseDate);
 }
