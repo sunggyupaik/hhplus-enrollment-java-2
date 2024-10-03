0fed3ff83c544542777dc426bd3046e7b56ef2a1
sunggyupaik <melon9751@naver.com>
2024-10-03 10:33:19 +0900

    [test] 신청자, 강의를 찾을 수 없는 경우를 테스트하라
    주어진 식별자로 신청자와 강의를 찾을 수 없는 경우
    존재하지 않는 예외를 반환하는 테스트를 추가합니다.

--- src/main/java/com/hanghe/enrollment/common/response/CommonControllerAdvice.java
+++ src/main/java/com/hanghe/enrollment/common/response/CommonControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.hanghe.enrollment.common.response;
 
-import com.hanghe.enrollment.common.exception.EntityNotFoundException;
+import com.hanghe.enrollment.common.exception.CourseNotFoundException;
+import com.hanghe.enrollment.common.exception.StudentNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,8 +11,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class CommonControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException e) {
+    @ExceptionHandler(CourseNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleCourseNotFoundException(CourseNotFoundException e) {
+        return ResponseEntity.status(400).body(new ErrorResponse("400", e.getMessage()));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(StudentNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleStudentNotFoundException(StudentNotFoundException e) {
         return ResponseEntity.status(400).body(new ErrorResponse("400", e.getMessage()));
     }
 }
